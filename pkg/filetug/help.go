0f5a7f6357db55d61236cf2964f4dafc7af9acde
Alexander Trakhimenok <alex@trakhimenok.com>
2026-01-13 16:32:20 +0000

    fix: F1 - help modal

--- pkg/filetug/help.go
+++ pkg/filetug/help.go
@@ -1,5 +1,75 @@
 package filetug
 
-func showHelpModal(nav *Navigator) {
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
 
+func showHelpModal(nav *Navigator) {
+	const helpText = `F1 - Help
+Alt+F - Favorites
+Alt+G - Go to...
+Al+P - Show/Hide previewer
+Alt+C - Copy files & directories
+Alt+M - Move files & directories
+Alt+D - Delete files & directories
+Alt+V - View file
+Alt+E - Edit file
+Alt+X - Exit the app`
+
+	helpView := tview.NewTextView().
+		SetDynamicColors(true).
+		SetText(helpText).
+		SetTextAlign(tview.AlignCenter)
+
+	helpView.SetBackgroundColor(tcell.ColorDarkBlue)
+	helpView.SetBorder(true).
+		SetTitle(" Help ").
+		SetTitleAlign(tview.AlignCenter)
+
+	// Create a modal-like layout using a Grid to center the helpView
+	// Close function
+	closeHelp := func() {
+		nav.app.SetRoot(nav.Flex, true)
+		nav.app.SetFocus(nav.dirsTree.TreeView)
+	}
+
+	helpView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyF1 {
+			closeHelp()
+			return nil
+		}
+		return event
+	})
+
+	// Add a button to close
+	button := tview.NewButton("Close").SetSelectedFunc(closeHelp)
+	button.SetBackgroundColor(tcell.ColorDarkBlue)
+	button.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyF1 {
+			closeHelp()
+			return nil
+		}
+		return event
+	})
+
+	// Update helpView to include the button or use a Flex
+	helpFlex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(helpView, 0, 1, false).
+		AddItem(button, 1, 0, true)
+
+	helpFlex.SetBorder(true).
+		SetTitle(" FileTug - Help ").
+		SetTitleAlign(tview.AlignCenter)
+	helpFlex.SetBackgroundColor(tcell.ColorDarkBlue)
+	helpView.SetBorder(false) // Border is now on helpFlex
+
+	// Update modal to use helpFlex
+	modal := tview.NewGrid().
+		SetColumns(0, 40, 0).
+		SetRows(0, 13, 0).
+		AddItem(helpFlex, 1, 1, 1, 1, 0, 0, true)
+
+	nav.app.SetRoot(modal, true)
 }
